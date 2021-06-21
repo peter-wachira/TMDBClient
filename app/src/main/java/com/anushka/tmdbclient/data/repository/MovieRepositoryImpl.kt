a94090b67b4fb6718e2b354b2ad77770a3b47eec
PeteHack <pwachira900@gmail.com>
2021-06-21 14:59:06 +0300

    Add implementation class for movie  resource data source, and add methods for updating cache and db

--- app/src/main/java/com/anushka/tmdbclient/data/repository/MovieRepositoryImpl.kt
+++ app/src/main/java/com/anushka/tmdbclient/data/repository/MovieRepositoryImpl.kt
@@ -11,27 +11,75 @@ class MovieRepositoryImpl (
     private val movieCacheDataSource: MovieCacheDataSource
         ) : MovieRepository{
     override suspend fun getMovies(): List<Movie>? {
-        TODO("Not yet implemented")
+        return  getMoviesFromCache()
     }
 
     override suspend fun updateMovies(): List<Movie>? {
-        TODO("Not yet implemented")
+        val newListOfMovies = getMoviesFromAPI()
+        movieLocalDataSource.clearAll()
+        movieLocalDataSource.saveMoviesToDB(newListOfMovies)
+        movieCacheDataSource.saveMoviesToCache(newListOfMovies)
+        return newListOfMovies
     }
 
     suspend fun  getMoviesFromAPI(): List<Movie>{
         lateinit  var movieList: List<Movie>
-
         try {
-            val response = movieRemoteDataSource.getMovies()
-            val body = response.body()
-            if (body!= null){
+          val response  = movieRemoteDataSource.getMovies()
+          val body = response.body()
+            if (body != null ){
                 movieList = body.movies
             }
-
         }catch (exception: Exception){
             Log.i("MYTAG",exception.message.toString() )
+        }
+
+        if(movieList.isNotEmpty()){
+            return  movieList
+        }
+        else {
+            movieList = getMoviesFromAPI()
+            movieLocalDataSource.saveMoviesToDB(movieList)
         }
         return movieList
     }
 
+    suspend fun getMoviesFromDB(): List<Movie>{
+        lateinit  var movieList: List<Movie>
+
+        try {
+            movieList = movieLocalDataSource.getMoviesFromDB()
+
+        }catch (exception: Exception){
+            Log.i("MYTAG",exception.message.toString())
+            if (movieList.isNotEmpty()){
+                return movieList
+            }else{
+                movieList = getMoviesFromAPI()
+                movieLocalDataSource.saveMoviesToDB(movieList)
+            }
+
+        }
+        return movieList
+    }
+
+    suspend fun getMoviesFromCache():List<Movie>
+    {
+        lateinit  var movieList: List<Movie>
+
+        try {
+           movieList = movieCacheDataSource.getMoviewsFromCache()
+        }catch (exception: Exception){
+            Log.i("MYTAG",exception.message.toString() )
+        }
+
+        if(movieList.isNotEmpty()){
+            return  movieList
+        }
+        else {
+            movieList = getMoviesFromAPI()
+            movieLocalDataSource.saveMoviesToDB(movieList)
+        }
+        return movieList
+    }
 }
