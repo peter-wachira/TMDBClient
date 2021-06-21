f8f0340df22f5e1b645f14b101ab5498a4758dae
PeteHack <pwachira900@gmail.com>
2021-06-21 16:15:07 +0300

    create packages for artist datasource and artist datasource implementation classes

---
+++ app/src/main/java/com/droid/tmdbclient/data/repository/artist/datasourceimpl/ArtistRemoteDataSourceImpl.kt
@@ -0,0 +1,19 @@
+package com.droid.tmdbclient.data.repository.artist.datasourceimpl
+
+import ArtistRemoteDataSource
+import com.droid.tmdbclient.data.api.TMDBService
+import com.droid.tmdbclient.data.model.artist.ArtistList
+import com.droid.tmdbclient.data.model.movie.MovieList
+import com.droid.tmdbclient.data.repository.movie.datasource.MovieRemoteDataSource
+
+import retrofit2.Response
+
+class ArtistRemoteDataSourceImpl (private val tmdbService: TMDBService, private val apiKey: String):
+    ArtistRemoteDataSource {
+
+    override suspend fun getArtists(): Response<ArtistList> {
+        return tmdbService.getPopularArtists(apiKey)
+    }
+
+
+}
