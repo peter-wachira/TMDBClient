08b7f0b4348214f101b0f51cd4003aabeacdfb6e
PeteHack <pwachira900@gmail.com>
2021-07-27 12:07:50 +0300

    Add Artists view model & view model factory
    Signed-off-by: PeteHack <pwachira900@gmail.com>

---
+++ app/src/main/java/com/droid/tmdbclient/presentation/artist/ArtistViewModel.kt
@@ -0,0 +1,22 @@
+package com.droid.tmdbclient.presentation.artist
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.droid.tmdbclient.domain.usecase.GetArtistsUseCase
+import com.droid.tmdbclient.domain.usecase.UpdateArtistsUseCase
+
+class ArtistViewModel(
+    private val getArtistsUseCase: GetArtistsUseCase,
+    private val updateArtistsUseCase: UpdateArtistsUseCase
+) : ViewModel() {
+
+    fun getArtists() = liveData {
+        val artistsList = getArtistsUseCase.execute()
+        emit(artistsList)
+    }
+
+    fun updateArtists() = liveData {
+        val artistsList = updateArtistsUseCase.execute()
+        emit(artistsList)
+    }
+}
