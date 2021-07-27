d06faa6102835c020f507984c5e84a597140a250
PeteHack <pwachira900@gmail.com>
2021-07-27 15:35:31 +0300

    change package name

---
+++ app/src/main/java/com/droid/tmdbclient/presentation/di/artist/ArtsitModule.kt
@@ -0,0 +1,19 @@
+package com.droid.tmdbclient.presentation.di.artist
+
+import com.droid.tmdbclient.domain.usecase.GetArtistsUseCase
+import com.droid.tmdbclient.domain.usecase.UpdateArtistsUseCase
+import com.droid.tmdbclient.presentation.artist.ArtistViewModelFactory
+import dagger.Module
+import dagger.Provides
+
+@Module
+class ArtistModule {
+
+    @Provides
+    fun provideArtistViewModelFactory(
+        getArtistsUseCase: GetArtistsUseCase,
+        updateArtistsUseCase: UpdateArtistsUseCase
+    ):ArtistViewModelFactory{
+        return ArtistViewModelFactory(getArtistsUseCase, updateArtistsUseCase)
+    }
+}
