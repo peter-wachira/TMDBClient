92241247d245503355e189963911d06989d0bc57
PeteHack <pwachira900@gmail.com>
2021-08-19 09:12:30 +0300

    Create App class that extends Application class
    Signed-off-by: PeteHack <pwachira900@gmail.com>

---
+++ app/src/main/java/com/droid/tmdbclient/presentation/App.kt
@@ -0,0 +1,37 @@
+package com.droid.tmdbclient.presentation
+
+import android.app.Application
+import com.droid.tmdbclient.BuildConfig
+import com.droid.tmdbclient.presentation.di.Injector
+import com.droid.tmdbclient.presentation.di.artist.ArtistSubComponent
+import com.droid.tmdbclient.presentation.di.core.*
+import com.droid.tmdbclient.presentation.di.movie.MovieSubComponent
+import com.droid.tmdbclient.presentation.di.tvshow.TvShowSubComponent
+
+class App : Application(), Injector {
+    private lateinit var appComponent: AppComponent
+
+    override fun createMovieSubComponent(): MovieSubComponent {
+        return appComponent.movieSubComponent().create()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerAppComponent.builder()
+            .appModule(AppModule(applicationContext))
+            .netModule(NetModule(BuildConfig.BASE_URL))
+            .remoteDataModule(RemoteDataModule(BuildConfig.API_KEY))
+            .build()
+
+    }
+
+    override fun createTvShowSubComponent(): TvShowSubComponent {
+        return appComponent.tvShowSubComponent().create()
+    }
+
+    override fun createArtistSubComponent(): ArtistSubComponent {
+        return appComponent.artsitSubComponent().create()
+
+    }
+
+}
